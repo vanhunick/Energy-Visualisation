0e15e0a3104540de2088174b2ac943b2ae8f63bf
Nicky van Hulst <nickyvanhulst@gmail.com>
2017-01-22 12:25:58 +1300

    A bar graph is generated when clicking on a row in the generated table

--- public/javascripts/bar.js
+++ public/javascripts/bar.js
@@ -5,67 +5,91 @@ var margin = { top: 60, right: 20, bottom: 30, left: 50 },
     width = 550 - margin.left - margin.right,
     height = 350 - margin.top - margin.bottom;
 
-var formatPercent = d3.format("0");
-
+// set the ranges
 var x = d3.scaleBand()
     .range([0, width])
     .padding(0.1);
 
 var y = d3.scaleLinear()
-    .range([height, 0]);
+    .range([height, 0])
+    .nice();
 
-function showYearBarGraph(dataInput) {
-    var x = d3.scaleBand()
-        .range([0, width])
-        .padding(0.1);
+var yAxis = d3.axisLeft();
 
-    var y = d3.scaleLinear()
-        .range([height, 0]);
 
-    var svg = d3.select("#bar").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var created = false;
+var svg = d3.select("#graph-div").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g") // group allows us to move everything together
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")"); // moves by a x and y value in this case the margins
 
-    var data = dataInput;
+function createBarGraph(title, data){
 
-    x.domain(data.map(function (d) { return d.letter; }));
-    y.domain([0, d3.max(data, function (d) { return d.frequency; })]);
+    // Add the graph to the graph div
+    // append the svg object to the body of the page
+    // append a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(d3.axisLeft(y))
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
+    // Scale the range of the data in the domains
+    // Goes through every element in the array and grabs the category (2011,2012,2013 etc)
+    x.domain(data.map(function(d) { return d.category; }));
 
-    svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) { return x(d.letter); })
-        .attr("width", x.bandwidth())
-        .attr("y", function (d) { return y(d.frequency); })
-        .attr("height", function (d) { return height - y(d.frequency); });
+    // The domain represents the min and max values of the data goes through all values and finds max
+    y.domain([0, d3.max(data, function(d) { return d.value; })]); // object should contain a value
+    y.nice();
+    if(created){
+        svg.selectAll(".bar") // None exist yet but will be created with enter
+            .data(data) // enter the data array
+    } else {
+        // Next step is to create the rectangles and add to the svg
+        svg.selectAll(".bar") // None exist yet but will be created with enter
+            .data(data) // enter the data array
+            .enter()
+            .append("rect") // create the rectangles
+            .attr("class", "bar") // add the class attribute
+            .attr("x", function(d) { return x(d.category); }) // set the x value
+            .attr("width", x.bandwidth()) // set the width of the bar
+            .attr("y", function(d) { return y(d.value); }) // set the y value according to the value
+            .attr("height", function(d) { return height - y(d.value); }); // set the height
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    svg.append("text")
-        .attr("x", (width / 2))
-        .attr("y", 0 - (margin.top / 2))
-        .attr("text-anchor", "middle")
-        .attr('class','graph-title')
-        .text("Messages sent and recieved per year");
-
-    function type(d) {
-        d.frequency = +d.frequency;
-        return d;
     }
+
+    yAxis.scale(y);
+
+    if(created){
+        svg.select(".yAxis")
+            .transition()
+            .duration(1000)
+            .call(yAxis);
+    } else{
+
+        // Now create the x and y axis
+        // add the x Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // add the y Axis
+        svg.append("g")
+            .attr("class","yAxis")
+            .call(yAxis);
+    }
+
+    if(created){
+        svg.select("#bar-title").text(title);
+    } else {
+        // Add a title
+        svg.append("text")
+            .attr("x", (width / 2))
+            .attr("y", 0 - (margin.top / 2))
+            .attr("id","bar-title")
+            .attr("text-anchor", "middle")
+            .style("font-size", "24px")
+            .text(title);
+    }
+
+    created = true;
 }
+
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -52,17 +52,12 @@ $(document).ready( function() {
         // Send category, section and company
         $.post("/sections/search",{section : section, category : category, company : company}, function(data){
             if(data.rows.length > 0){
-                console.log("insering table")
                 insertDataTable(data.rows);
             } else {
                 console.log("not inserting table");
             }
         });
-
-
     });
-
-
 });
 
 function insertDataTable(rows){
@@ -85,7 +80,7 @@ function insertDataTable(rows){
         years += "<th>" + i + "</th>";
     }
 
-    $('#results-table').append('<tr> <th>EDB</th>'+ years + '</tr>');
+    $('#results-table').append('<tr id="head-row"> <th>EDB</th>'+ years + '</tr>');
 
 
     // An array of companies already processed
@@ -97,8 +92,8 @@ function insertDataTable(rows){
 
             done.push(rows[i].edb);
 
-            // Insert name in column
-            var row = "<tr><th>" + rows[i].edb + "</th>";
+            // Insert name in column and assign an id to the row
+            var row = "<tr id=row"+i+"><th>" + rows[i].edb + "</th>";
 
             for(var cur = min.disc_yr; cur <=max.disc_yr; cur++){
 
@@ -115,13 +110,46 @@ function insertDataTable(rows){
             //console.log("Data Row " + row);
             $('#results-table').append(row + '</tr>');
 
+            // Assing a on click function to each of the rows to generate the bar graph with the row specific data
+            $( "#row"+i+"").click(function(event) {
+                showBarWithRowElem(this.id);
+            });
+
         }
     }
 }
 
+function showBarWithRowElem(rowID){
+    var data = [];
+
+
+    // set the row selected
+    $('#results-table').find('tr').removeClass('row-selected');
+    $('#'+rowID).addClass('row-selected');
+
+
+    $('#head-row').find('th').each(function (index, element) {
+        if(index != 0){ // 0 is not a year
+            data.push({category : $(element).text(), value : 0}); // 0 is temp
+        }
+    });
+
+    var title = "";
+
+    $('#'+rowID).find('th').each(function (index, element) {
+        if(index != 0){
+            data[index-1].value = $(element).text();
+        } else {
+            title = data[index].value = $(element).text();
+        }
+    });
+
+    createBarGraph(title, data);
+}
+
+
 function insertCompanies(){
     $.get("/sections/company", function(data){
-        console.log(data);
         if(data.companies.length > 0){
             $('#company-select').html('<option selected>' + data.companies[0] + '</option>'); // Empty temp options
         }
