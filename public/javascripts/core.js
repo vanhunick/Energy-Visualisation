493739fde14b6a6fb0a5414b3aeb6cb936a315f6
Nicky van Hulst <nickyvanhulst@gmail.com>
2017-01-28 15:34:09 +1300

    Started adding validation to cpi fields

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -2,4 +2,37 @@ $(document).ready( function() {
     // Highlight the selected link
     $(".nav-link").removeClass('active');
     $("#core-link").addClass('active');
+
+
+    var cpiRules = {
+        required : true,
+        number : true,
+        min : 0,
+        max : 100
+    };
+
+    var messageSet = {
+        required : "Please enter a percentage",
+        number : "Please enter a valid number",
+        min : "Please enter a percentage greater than 0",
+        max : "Please enter a percentage less than 100"
+    }
+
+    $('#cpi-form').validate({
+        rules : {
+            Y2012 : cpiRules,
+            Y2013 : cpiRules,
+            Y2014 : cpiRules,
+            Y2015 : cpiRules,
+            Y2016 : cpiRules
+        },
+        messages : {
+            Y2012 : messageSet,
+            Y2013 : messageSet,
+            Y2014 : messageSet,
+            Y2015 : messageSet,
+            Y2016 : messageSet
+        }
+    });
+
 });
