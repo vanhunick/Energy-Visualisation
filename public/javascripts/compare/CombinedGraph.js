27f33c9b14371b915307fdbdfd346705b3825ac9
Nicky van Hulst <nickyvanhulst@gmail.com>
2017-04-07 11:22:27 +1200

    Added documentation and titles to combined Graphs

--- public/javascripts/compare/CombinedGraph.js
+++ public/javascripts/compare/CombinedGraph.js
@@ -1,3 +1,15 @@
+// DEPENDENCIES : DataProcessor, GroupedBarGraph
+
+/**
+ * Object that handles logic for updating and creating all of the graphs on the dashboard across the tabs
+ *
+ * @param id {String} the id of the selection
+ * @param combinedRows {Object[]} the result of rows1 divided by rows2
+ * @param rows1 {Object[]} the rows for the first selection
+ * @param rows2 {Object[]} the rows for the selection selection
+ * @param selection1 {Object} the first search
+ * @param selection2 {Object} the second search
+ * */
 function CombinedGraph(id, combinedRows, rows1,rows2,selection1,selection2){
   this.id = id;
   this.groupedID = '#grouped-bar'+id;
@@ -9,40 +21,88 @@ function CombinedGraph(id, combinedRows, rows1,rows2,selection1,selection2){
   this.selection1 = selection1;
   this.selection2 = selection2;
 
-  this.isCombined = function () {
-    return true;
-  }
 
-  this.getID = function () {
-    return this.id;
-  }
-
-  // Chache dom elements
+  // Cache dom elements
   this.$titleBar = $('#title-bar-'+id);
   this.$titleBox = $('#title-box-'+id);
   this.$titleVector = $('#title-vector-'+id);
 }
 
-CombinedGraph.prototype.getTitle = function () {
-  return this.selection.description + ", " + this.selection.subCategory;
+
+/**
+ * Returns if the the object is combined or not
+ *
+ * @return {Boolean} true if combined
+ * */
+CombinedGraph.prototype.isCombined = function() {
+  return true;
 }
 
-// TODO check if these should be updated like this since they are based on others
-CombinedGraph.prototype.applyCPI = function (cpiValues) {
-  applyCPIToTableRows(this.rows, cpiValues);
-  applyCPIToTableRows(this.rows1, cpiValues);
-  applyCPIToTableRows(this.rows2, cpiValues);
-  this.create();
+
+/**
+ * Returns the id of the combined graphs
+ *
+ * @return {String} the id of the combined graphs
+ * */
+CombinedGraph.prototype.getID = function () {
+  return this.id;
 }
 
-CombinedGraph.prototype.getSubTitle = function () {
-  return this.selection.section + ", " + this.selection.category;
+
+/**
+ * Returns the title of the first selection
+ *
+ * @return {String} the title of the first selection
+ * */
+CombinedGraph.prototype.getSelection1Title = function () {
+   var selection1Title = this.selection1.description + ", " + this.selection1.category;
+
+
+  return selection1Title;
 }
 
+
+/**
+ * Returns the title of the second selection
+ *
+ * @return {String} the title of the second selection
+ * */
+CombinedGraph.prototype.getSelection2Title = function () {
+  var selection2Title = this.selection2.description + ", " + this.selection2.category;
+
+  return selection2Title;
+}
+
+
+/**
+ * Returns the unit for the combined graph
+ *
+ * @return {String} the combined units for the selections
+ * */
 CombinedGraph.prototype.getUnit = function () {
-  return this.rows1[0].unit + " / " + rows2[0].unit;
+  return this.rows1[0].unit + " / " + this.rows2[0].unit;
 }
 
+
+/**
+ * Inserts the titles for the graphs into the DOM elements
+ * */
+CombinedGraph.prototype.inserTitles = function () {
+  var titleHTML = '<h4 class="combined-title">'+this.getSelection1Title()+'<br><span class="over">over</span><br>'+this.getSelection2Title()+'</h4>'
+
+  this.$titleBar.append(titleHTML);
+  this.$titleBox.append(titleHTML);
+  this.$titleVector.append(titleHTML);
+}
+
+
+/**
+ * Creates the Graphs
+ *
+ * @param rows the new combined rows
+ * @param the new rows for the first selection
+ * @param rows2 the new rows for the second selection
+ * */
 CombinedGraph.prototype.create = function () {
     // First insert titles
 
@@ -59,6 +119,13 @@ CombinedGraph.prototype.create = function () {
     createVectorGraph(vectorGraphData,selection.unit1,selection.unit2,this.vectorID);
 }
 
+/**
+ * Called when the rows are updated by CPI
+ *
+ * @param rows the new combined rows
+ * @param the new rows for the first selection
+ * @param rows2 the new rows for the second selection
+ * */
 CombinedGraph.prototype.update = function (rows, rows1,rows2) {
   this.rows = rows;
   this.rows1 = rows1;
