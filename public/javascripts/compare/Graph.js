bcd56fef033f975651692460812d9a33cccb05e3
Nicky van Hulst <nickyvanhulst@gmail.com>
2017-04-07 12:17:01 +1200

    Documented Graph.js and split up create and create titles

--- public/javascripts/compare/Graph.js
+++ public/javascripts/compare/Graph.js
@@ -1,4 +1,14 @@
-function Graphs(id,rows,selection) {
+// DEPENDENCIES : DataProcessor, GroupedBarGraph, boxPlot
+
+
+/**
+ * Graph object to control the graphs for single selections
+ *
+ * @id the id of the selection
+ * @rows {Object[]} the rows for the selection
+ * @selection {Object} the details of the selection
+ * */
+function Graph(id,rows,selection) {
   this.id = id;
   this.groupedID = '#grouped-bar-'+id;
   this.boxID = '#boxplot-'+id;
@@ -6,42 +16,87 @@ function Graphs(id,rows,selection) {
   this.selection = selection;
   this.title = this.getTitle();
   this.subTitle = this.getSubTitle();
-
-  this.isCombined = function () {
-    return false;
-  }
-
-  this.getID = function () {
-    return this.id;
-  }
+  this.unit = this.getUnit();
 
   // Cache dom elements
   this.$titleBar = $('#title-bar-'+id);
   this.$titleBox = $('#title-box-'+id);
-
-  this.getUnit = function () {
-    return this.combined ? "two units " : "one unit";
-  }
-  this.unit = this.getUnit();
 }
 
-Graphs.prototype.getTitle = function () {
+
+/**
+ * Returns if the object is combined
+ *
+ * @return {Boolean} combined or not
+ * */
+Graph.prototype.isCombined = function () {
+  return false;
+}
+
+
+/**
+ * Returns the unit of the graph
+ *
+ * @return {String} the unit
+ * */
+Graph.prototype.getUnit = function () {
+  return this.rows[0].units;
+}
+
+
+/**
+ * Returns the id of the graph
+ *
+ * @return {String} the id
+ * */
+Graph.prototype.getID = function () {
+  return this.id;
+}
+
+
+/**
+ * Returns the title for the graphs
+ *
+ * @return {String} the title
+ * */
+Graph.prototype.getTitle = function () {
   return this.selection.description + ", " + this.selection.subCategory;
 }
 
-Graphs.prototype.getSubTitle = function () {
+
+/**
+ * Returns the sub title for the graphs
+ *
+ * @return {String} the sub title
+ * */
+Graph.prototype.getSubTitle = function () {
   return this.selection.section + ", " + this.selection.category;
 }
 
-Graphs.prototype.getUnit = function () {
-  return this.rows[0].unit;
+
+/**
+ * Returns the unit for the graphs
+ *
+ * @return {String} the unit
+ * */
+Graph.prototype.getUnit = function () {
+  return this.rows[0].units;
 }
 
-Graphs.prototype.create = function () {
-    // First add the titles
-    this.$titleBar.append('<h3 class="title">'+this.title+'</h3>').append('<h4 class="subTitle">'+this.subTitle+'</h4>');
-    this.$titleBox.append('<h3 class="title">'+this.title+'</h3>').append('<h4 class="subTitle">'+this.subTitle+'</h4>');
 
+/**
+ * Inserts the titles for the graphs into the DOM
+ * */
+Graph.prototype.insertTitles = function () {
+  this.$titleBar.append('<h3 class="title">'+this.title+'</h3>').append('<h4 class="subTitle">'+this.subTitle+'</h4>');
+  this.$titleBox.append('<h3 class="title">'+this.title+'</h3>').append('<h4 class="subTitle">'+this.subTitle+'</h4>');
+}
+
+
+/**
+ * Creates the data using data processor and inserts into DOM
+ * */
+Graph.prototype.create = function () {
     // Create the data for the grouped graph and display it
     var groupedData = dp.createDataForGroupedGraph(this.rows);
     createdGroupedBarGraph(groupedData.data,groupedData.keys,this.unit,this.groupedID);
@@ -52,14 +107,12 @@ Graphs.prototype.create = function () {
 }
 
 
-Graphs.prototype.applyCPI = function (cpiValues) {
-  if(this.canApplyCPI()){
-    applyCPIToTableRows(this.row, cpiValues);
-    this.create();
-  }
-}
-
-Graphs.prototype.update = function (rows) {
+/**
+ * Takes the new rows and updates the graphs
+ *
+ * @param {Object[]} rows the new rows
+ * */
+Graph.prototype.update = function (rows) {
     this.rows = rows;
     this.create();
 }
